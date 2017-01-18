9b86af27250327713ff4981f1005e277f0deb960
Vicente González Ruiz <vruiz@ual.es>
2017-01-18 16:38:00 +0100

    .

---
+++ MCDWT.py
@@ -0,0 +1,12 @@
+n = 9 # Number of images
+l = 3 # Number of temporal scales
+
+x = 2
+for l in range(3): # Number of temporal scales
+    i = 0
+    while i < (n//x):
+        print('A = ', x*i)
+        print('B = ', x*i+x//2)
+        print('C = ', x*i+x)
+        i += 1
+    x *= 2
