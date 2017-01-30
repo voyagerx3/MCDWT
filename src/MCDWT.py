64d1034aae06522f513a35e0062c7be4a33d62d9
Vicente González Ruiz <vruiz@ual.es>
2017-01-30 09:33:13 +0100

    .

--- src/MCDWT.py
+++ src/MCDWT.py
@@ -1,11 +1,13 @@
 def MCDWT(input = 'input', output = 'output', n=5, l=2) -> None :
-    ''' A Motion Compendated Discrete Wavelet Transform.
+    ''' A Motion Compensated Discrete Wavelet Transform.
+
+    Compute the 1D-DWT along motion trajectories. The input video (as a sequence of images) must be stored in disk (<input> directory) and the output (as a sequence of DWT coefficients that are called pyramids) will be stored in disk (<output> directory). So, this MCDWT implementation does not transform the video on the fly.
 
     Arguments:
-        input: directory of the input images that must be named 000.png, 001.png, etc.
-        output: directory fo the output (transformed) pyramids (named 000.png, 001.png, etc.).
-        n: number of images of the input.
-        l: number of leves of the MCDWT (temporal scales). Controls the GOP size. Examples: l=0 -> GOP_size = 1, l=1 -> GOP_size = 2, l=2 -> GOP_size = 4. etc.
+        <input>: directory of the input images that must be named 000.png, 001.png, etc.
+        <output>: directory fo the output (transformed) pyramids (named 000.png, 001.png, etc.).
+        <n>: number of images of the input.
+        <l>: number of leves of the MCDWT (temporal scales). Controls the GOP size. Examples: l=0 -> GOP_size = 1, l=1 -> GOP_size = 2, l=2 -> GOP_size = 4. etc.
     '''
 
     x = 2
