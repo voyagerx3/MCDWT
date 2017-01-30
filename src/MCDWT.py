269291a51bcd215f21ea570d94c62a97dc76b6ec
Vicente González Ruiz <vruiz@ual.es>
2017-01-30 07:52:27 +0100

    .

--- src/MCDWT.py
+++ src/MCDWT.py
@@ -3,7 +3,7 @@ def MCDWT(input = 'input', output = 'output', n=5, l=2) -> None :
 
     Arguments:
         input: directory of the input images that must be named 000.png, 001.png, etc.
-        output: directory fo the output images (named 000.png, 001.png, etc.).
+        output: directory fo the output (transformed) pyramids (named 000.png, 001.png, etc.).
         n: number of images of the input.
         l: number of leves of the MCDWT (temporal scales). Controls the GOP size. Examples: l=0 -> GOP_size = 1, l=1 -> GOP_size = 2, l=2 -> GOP_size = 4. etc.
     '''
