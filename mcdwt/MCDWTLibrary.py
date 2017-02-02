ee55e8397ecb10205155531a2e1f095375044e59
vicmagv <vicmagv@gmail.com>
2017-02-02 19:44:58 +0100

    refactoring

--- mcdwt/MCDWTLibrary.py
+++ mcdwt/MCDWTLibrary.py
@@ -35,95 +35,51 @@ def read_frame(filename):
 
     return cv2.imread(filename,1)
 
-def image_to_dwt2d(image):
-    '''
-    Compute the 1-level 2D-DWT of a frame in memory.
-    '''
+
+def image_y_to_four_bands(image):
     coeffs = pywt.dwt2(image, 'haar')
-    
-    return coeffs
+    return coeffs[0], coeffs[1][0], coeffs[1][1], coeffs[1][2],
 
+def generate_x(ll, hl, lh, hh): 
+    height = ll.shape[0]*2
+    width = ll.shape[1]*2
+    z = np.zeros((height//2, width//2), dtype="float64")
+    il = pywt.idwt2((ll, (z, z, z)), 'haar')
+    ih = pywt.idwt2((z, (hl, lh, hh)), 'haar')
+    return il, ih
 
-
-def dwt2d_to_image(coeffs):
-    '''
-    Compute to a frame the 1-level 2D-DWT
-    '''
-    image = pywt.idwt2(coeffs, 'haar')
-    
-    return image
+def output_from_dwt(ll, hl, lh, hh): 
+    height = ll.shape[0]*2
+    width = ll.shape[1]*2
+    output = np.zeros((height, width), dtype="int16")
+    output[0:height//2, 0:width//2] = ll
+    output[0:height//2, width//2:width] = hl
+    output[height//2:height, 0:width//2] = lh
+    output[height//2:height, width//2:width] = hh
+    return output
 
 def forward_MCDWT(imageA, imageB, imageC):
 
-    height = imageA.shape[0]
-    width = imageA.shape[1]
-    coeffA=image_to_dwt2d(imageA[:,:,0])
-    coeffB=image_to_dwt2d(imageB[:,:,0])
-    coeffC=image_to_dwt2d(imageC[:,:,0])
-    
-    All = coeffA[0]
-    Ahl = coeffA[1][0]
-    Alh = coeffA[1][1]
-    Ahh = coeffA[1][2]
-
-    Bll = coeffB[0]
-    Bhl = coeffB[1][0]
-    Blh = coeffB[1][1]
-    Bhh = coeffB[1][2]
-
-    Cll = coeffC[0]
-    Chl = coeffC[1][0]
-    Clh = coeffC[1][1]
-    Chh = coeffC[1][2]
-    
-    zeroes = np.zeros((height//2, width//2), dtype="float64")
+    All, Ahl, Alh, Ahh = image_y_to_four_bands(imageA[:,:,0],)
+    Bll, Bhl, Blh, Bhh = image_y_to_four_bands(imageB[:,:,0],)
+    Cll, Chl, Clh, Chh = image_y_to_four_bands(imageC[:,:,0],)
         
-    iAl = dwt2d_to_image((All,(zeroes,zeroes,zeroes)))
-    iAh = dwt2d_to_image((zeroes,(Ahl,Alh,Ahh)))
+    iAl, iAh = generate_x(All, Ahl, Alh, Ahh) 
+    iBl, iBh = generate_x(Bll, Bhl, Blh, Bhh) 
+    iCl, iCh = generate_x(Cll, Chl, Clh, Chh) 
 
-
-    iBl = dwt2d_to_image((Bll,(zeroes,zeroes,zeroes)))
-    iBh = dwt2d_to_image((zeroes,(Bhl,Blh,Bhh)))
-
-
-    iCl = dwt2d_to_image((Cll,(zeroes,zeroes,zeroes)))
-    iCh = dwt2d_to_image((zeroes,(Chl,Clh,Chh)))
-
-
-    imagenResiduo = iBh-((iAh + iCh)/2)
-
-    coeffR = image_to_dwt2d(imagenResiduo)
-    
-    Rll = coeffR[0]
-    Rhl = coeffR[1][0]
-    Rlh = coeffR[1][1]
-    Rhh = coeffR[1][2]
-
+    waste_image = iBh-((iAh + iCh)/2)
+    Rll, Rhl, Rlh, Rhh = image_y_to_four_bands(waste_image)
     Rll = Bll
-
-    outputA = np.zeros((height, width), dtype="int16")
-    outputA[0:height//2, 0:width//2] = All
-    outputA[0:height//2, width//2:width] = Ahl
-    outputA[height//2:height, 0:width//2] = Alh
-    outputA[height//2:height, width//2:width] = Ahh
-
-    outputC = np.zeros((height, width), dtype="int16")
-    outputC[0:height//2, 0:width//2] = Cll
-    outputC[0:height//2, width//2:width] = Chl
-    outputC[height//2:height, 0:width//2] = Clh
-    outputC[height//2:height, width//2:width] = Chh
-
-    outputR = np.zeros((height, width), dtype="int16")
-    outputR[0:height//2, 0:width//2] = Rll
-    outputR[0:height//2, width//2:width] = Rhl
-    outputR[height//2:height, 0:width//2] = Rlh
-    outputR[height//2:height, width//2:width] = Rhh
-
-    # cv2.imwrite('test_images/A.png',outputA)
-    # cv2.imwrite('test_images/R.png',outputR)
-    # cv2.imwrite('test_images/C.png',outputC)
+    outputA = output_from_dwt(All, Ahl, Alh, Ahh)
+    outputR = output_from_dwt(Rll, Rhl, Rlh, Rhh)
+    outputC = output_from_dwt(Cll, Chl, Clh, Chh)
 
     return outputA, outputR, outputC
+
+def normalize_uint16_to_uint8(image):
+    image = 256*((image+512)/1024)
+    return np.uint8(image)
 
 def video_converter (file_in, file_out):
 
@@ -146,35 +102,11 @@ def video_converter (file_in, file_out):
         image3 = cv2.cvtColor(frame3, cv2.COLOR_BGR2YCrCb)
 
         image1, image2, image3 = forward_MCDWT(image1,image2,image3)
-        
-        image1 = 256*((image1+512)/1024)
-        image2 = 256*((image2+512)/1024)
-        image3 = 256*((image3+512)/1024)
 
-        image1 = np.uint8(image1)
-        image2 = np.uint8(image2)
-        image3 = np.uint8(image3)
-
-
-        out.write(image1)
-        out.write(image2)
-        out.write(image3)
+        out.write(normalize_uint16_to_uint(image1))
+        out.write(normalize_uint16_to_uint(image2))
+        out.write(normalize_uint16_to_uint(image3))
         frame1 = frame3
 
     cap.release()
     out.release()
-
-# forward_MCDWT(read_frame('output/PNG/imagen56.png'),read_frame('output/PNG/imagen57.png'),read_frame('output/PNG/imagen58.png'))
-
-# read_video('stockholm_1280x768x50x420x578.avi')
-
-# image = read_frame('output/stockholm_1280x768x50x420x578.avi13.npy')   
-# cv2.imshow('image', image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# image = read_frame('output/stockholm_1280x768x50x420x578.avi13.npy') 
-# image = dwt2d_to_image(image_to_dwt2d(image))  
-# cv2.imshow('image', image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
