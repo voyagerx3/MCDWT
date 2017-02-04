2cd9c961f6fb5967f08d0ee4a42e97077b632c03
vicente-gonzalez-ruiz <vicente.gonzalez.ruiz@gmail.com>
2017-02-04 20:12:28 +0100

    .

--- mcdwt/__main__.py
+++ mcdwt/__main__.py
@@ -1,6 +1,6 @@
 import transform_step
 
 #mcdwt.MCDWT('test_images/','/tmp/',5,1)
-transform_step.MCDWT('/tmp/eq/','/tmp/',5,1)
-transform_step.iMCDWT('/tmp/','/tmp/res',5,1)
+transform_step.forward('/tmp/eq/','/tmp/',5,1)
+transform_step.backward('/tmp/','/tmp/res',5,1)
 
