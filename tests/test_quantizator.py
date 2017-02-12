bbf06fa761b02d24ecbac9efbf215c8d24d28c25
Vicente González Ruiz <vruiz@ual.es>
2017-02-12 11:31:41 +0100

    Creating tools and show_histogram.sh

--- tests/test_quantizator.py
+++ tests/test_quantizator.py
@@ -3,12 +3,12 @@ import numpy as np
 
 from mcdwt.quantizator import quantizator, unQuantizator
 
-coef = 2
+coef = 64
 
 frame = cv2.imread("../images/001.png")
 outputQuantizated = quantizator(frame, coef)
 
-cv2.imwrite("../images/001quantizates.png", outputQuantizated)
+cv2.imwrite("/tmp/001_quantized.png", outputQuantizated)
 
 outputUnQuantizated = unQuantizator(outputQuantizated, coef)
-cv2.imwrite("../images/001unQuantizates.png", outputUnQuantizated)
+cv2.imwrite("/tmp/001_unquantized.png", outputUnQuantizated)
