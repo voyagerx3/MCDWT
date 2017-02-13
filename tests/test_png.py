54423c0f34320760fb05ae29b7f3c77ab6cfb5c0
Raúl García M <raulgarmu@gmail.com>
2017-02-13 13:53:28 +0100

    -Reverted test_png.py. -Added image color conversion scripts in tools. -Removed test_color_converter.py from tests. -Removed rgb_ycc_coonvesion.py from mcdwt.

--- tests/test_png.py
+++ tests/test_png.py
@@ -1,30 +1,17 @@
-import mcdwt.image_io as imgio
+import cv2
 import numpy as np
-import unittest
 
+image = (np.random.rand(256, 256) * 255).astype('uint16')
+cv2.imwrite('/tmp/a.png', image)
+image2 = cv2.imread('/tmp/a.png', -1)
+(image == image2).all()
+print(type(image[0][0]))
+print(type(image2[0][0]))
 
-class TestImageMethods(unittest.TestCase):
-    def test_read_write(self):
-        reader = imgio.ImageReader()
-        writer = imgio.ImageWritter()
+image = (np.random.rand(256, 256) * 255).astype('int16')
+cv2.imwrite('/tmp/a.png', image)
+image2 = cv2.imread('/tmp/a.png',-1)
+(image == image2).all()
+print(type(image[0][0]))
+print(type(image2[0][0]))
 
-        # Create a random 'image'
-        im_uint = (np.random.rand(256, 256) * 255).astype('uint16')
-        im_int = (np.random.rand(256, 256) * 255).astype('int16')
-
-        # Write the image
-        writer.write(im_uint, 0, '../images/output/uint_')
-        writer.write(im_int, 0, '../images/output/int_')
-
-        # Read the image
-        im_rd_uint = reader.read(0, '../images/output/uint_')
-        im_rd_int = reader.read(0, '../images/output/int_')
-
-        # Assert
-        self.assertIsNotNone(im_rd_uint, msg="Fail to read the image")
-        self.assertIsNotNone(im_rd_int, msg="Fail to read the image")
-        self.assertEquals(im_uint.all(), im_rd_uint.all(), msg="Fail to check image integrity")
-        self.assertEquals(im_int.all(), im_rd_int.all(), msg="Fail to check image integrity")
-
-if __name__ == '__main__':
-    unittest.main()
