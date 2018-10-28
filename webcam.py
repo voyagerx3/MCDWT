9d2e2fd857aab9f174bd1bf9a1100b7506fd495a
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2018-10-28 20:11:28 +0100

    .

--- webcam.py
+++ webcam.py
@@ -4,34 +4,51 @@ import cv2
 from time import time
 import sys
 
-cap = cv2.VideoCapture(0)
-width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-n_labels = width * height * 3
-fps = cap.get(cv2.CAP_PROP_FPS)
-sys.stdout.write("Capturing from webcam at " + str(width) + 'x' + str(height) + " pixels, " + str(fps) + " Hz\n")
+class WebCam():
 
-counter = 0
-start = time()
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    def process(self, frame):        
+        return frame
 
-    # Display the resulting frame
-    cv2.imshow('WebCam', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    runtime = time() - start
-    fps = counter/runtime
-    counter += 1
-    value, counts = np.unique(frame, return_counts=True)
-    probs = counts / n_labels
-    ent = 0.
-    for i in probs:
-        ent -= i * log(i, 2.0)
-    sys.stdout.write("Frame={:04d} FPS={:2.1f} Entropy={:1.1f}\r".format(counter, fps, ent))
-    sys.stdout.flush()
-    
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    def run(self):
+        cap = cv2.VideoCapture(0)
+        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        n_labels = width * height * 3
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        sys.stdout.write("Capturing from webcam at " + str(width) + 'x' + str(height) + " pixels, " + str(fps) + " Hz\n")
+
+        counter = 0
+        start = time()
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+
+            # Process the frame
+            frame = self.process(frame)
+
+            # Display the resulting frame
+            cv2.imshow('WebCam', frame)
+
+            # Compute some stats on processed frame
+            end = time()
+            fps = counter/(end - start)
+            counter += 1
+            value, counts = np.unique(frame, return_counts=True)
+            probs = counts / n_labels
+            ent = 0.
+            for i in probs:
+                ent -= i * log(i, 2.0)
+            sys.stdout.write("Frame={:04d} FPS={:2.1f} Entropy={:1.1f}\r".format(counter, fps, ent))
+            sys.stdout.flush()
+
+            # Exit?
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    driver = WebCam()
+    driver.run()
--- webcam_2DDWT.py
+++ webcam_2DDWT.py
@@ -2,24 +2,18 @@ import numpy as np
 import cv2
 #from filters.copy_frame import process_frame
 from transform.dwt import forward, backward
-cap = cv2.VideoCapture(0)
+from webcam import WebCam
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+class WebCam_2DDWT(WebCam):
+    def process(self, frame):
+        LL, H = forward(frame)
+        cv2.imshow('LL', LL.astype(np.uint8))
+        cv2.imshow('LH', H[0].astype(np.uint8)*16+128)
+        cv2.imshow('HL', H[1].astype(np.uint8)*16+128)
+        cv2.imshow('HH', H[2].astype(np.uint8)*16+128)
+        recons = backward(LL, H)
+        return recons.astype(np.uint8)
 
-    LL, H = forward(frame)
-    cv2.imshow('LL', LL.astype(np.uint8))
-    cv2.imshow('LH', H[0].astype(np.uint8)*16+128)
-    cv2.imshow('HL', H[1].astype(np.uint8)*16+128)
-    cv2.imshow('HH', H[2].astype(np.uint8)*16+128)
-    recons = backward(LL, H)
-
-    # Display the resulting frame
-    cv2.imshow('WebCam', recons.astype(np.uint8))
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    driver = WebCam_2DDWT()
+    driver.run()
